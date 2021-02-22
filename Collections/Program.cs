1bb2b6467808d346685aae6a591ea798c856b4cf
mustafa şeker <mustiseker.06@gmail.com>
2021-02-23 00:13:25 +0300

    Add project files.

---
+++ Collections/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "mustafa","gizem","seda","züleyha" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+
+            //// new dediğimiz anda yeni bir array bellekte  oluşur 00 tertemiz
+            //isimler = new string[5];
+
+            //isimler[4] = "ayşe";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            // ama listelerde işler değişiyor
+            List<string> isimler2 = new List<string> { "mustafa", "gizem", "seda", "züleyha" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("ilker");
+            Console.WriteLine(isimler2[4]);
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mylist<string> isimler = new Mylist<string>();
+            isimler.Add("mustafa");
+
+            Console.WriteLine(isimler.Length);
+
+            isimler.Add("ali");
+
+            Console.WriteLine(isimler.Length);
+
+        }
+    }
+}
