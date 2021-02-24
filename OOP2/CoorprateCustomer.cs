c34962f67838285dfcf24c676aea4a1b97876eee
mmustafaseker <mustiseker.06@gmail.com>
2021-02-25 00:36:57 +0300

    OOP2-Added

---
+++ OOP2/CoorprateCustomer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    // Tüzel müşteri
+    //inheritance - miras
+    class CoorprateCustomer : Customer
+    {
+        public string CompanyName { get; set; }
+        public string TaxNumber { get; set; }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mustafa Şeker
+
+            IndividualCustomer customer1 = new IndividualCustomer();
+            customer1.Id = 1;
+            customer1.CustomerNumber = "12345";
+            customer1.FirstName = "Mustafa";
+            customer1.LastName = "Şeker";
+            customer1.TcNumber = "123456789";
+
+            //bilgeAdam
+
+            CoorprateCustomer customer2 = new CoorprateCustomer();
+            customer2.Id = 2;
+            customer2.CustomerNumber = "45321";
+            customer2.CompanyName = "BilgeAdam";
+            customer2.TaxNumber = "12345678965";
+
+            //önemli bir nokta
+            Customer customer3 = new IndividualCustomer();
+            Customer customer4 = new CoorprateCustomer();
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+            //gerçek müşteri - tüzel müşteri
+            //SOLID
+        }
+    }
+}
