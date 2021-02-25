aa0141e44c5ad123b20b5bd625fed3696816fa02
mmustafaseker <mustiseker.06@gmail.com>
2021-02-25 14:46:59 +0300

    OOP3-Added

---
+++ OOP3/BasvuruManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManager
+    {
+        //birden çok loglanma
+        public void BasvuruYap(ICreditManager creditManager, List<ILoggerService> loggerServices)
+        {
+            //Başvuru bigilerini değerlendirme
+            //
+
+            creditManager.Hesapla();
+            foreach (var loggerService in loggerServices)
+            {
+                loggerService.Log();
+            }
+        }
+
+        public void KrediOnBilgilendirmesiYap(List<ICreditManager> credits)
+        {
+            foreach (var credit in credits)
+            {
+                credit.Hesapla();
+            }
+        }
+
+    }
+}
--- OOP3/Program.cs
+++ OOP3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP3
 {
@@ -6,7 +7,36 @@ namespace OOP3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // open close prensip
+            // yazılımda sürdürülebilirlik
+            //interface'lerde implement edilen sınıfın referansını tutabiliyor
+            ICreditManager ihtiyacCreditManager = new IhtiyacCreditManager();
+            ICreditManager tasitCreditManager = new TasitCreditManager();
+            ICreditManager konutCreditManager = new KonutCreditManager();
+
+            ILoggerService dataBaseLoggerService = new DataBaseLoggerService();
+            ILoggerService fileLoggerService = new FileBaseLoggerService();
+            // 1
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ihtiyacCreditManager , new List<ILoggerService> {dataBaseLoggerService,new SmsLoggerService() });
+            // 2
+            BasvuruManager basvuruManager1 = new BasvuruManager();
+            basvuruManager1.BasvuruYap(konutCreditManager, new List<ILoggerService> { fileLoggerService, new SmsLoggerService() });
+            // 3
+            BasvuruManager basvuruManager2 = new BasvuruManager();
+            basvuruManager2.BasvuruYap(new EsnafCreditManager(), new List<ILoggerService> { dataBaseLoggerService, new SmsLoggerService() });
+
+
+            // 4
+            List<ILoggerService> logger = new List<ILoggerService> { new SmsLoggerService(), new DataBaseLoggerService(), new FileBaseLoggerService() };
+            BasvuruManager basvuruManager3 = new BasvuruManager();
+            basvuruManager.BasvuruYap(konutCreditManager, logger);
+
+            List<ICreditManager> credits = new List<ICreditManager>(){ihtiyacCreditManager, tasitCreditManager };
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(credits);
+    
+
         }
     }
 }
